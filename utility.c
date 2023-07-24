6d8f8d4077021199c5156a3bf62e7493d59df152
mbarleesah <palesasthembiso94@gmail.com>
2023-07-24 14:47:36 -0700

    c is not fun

--- utility.c
+++ utility.c
@@ -73,7 +73,7 @@ int print_rot13(va_list p, flags_t *m)
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *t = va_arg(p, char *);
 
-	(void)f;
+	(void)m;
 	for (l = 0; t[l]; l++)
 	{
 		if (t[l] < 'A' || (t[l] > 'Z' && t[l] < 'a') || t[l] > 'z')
